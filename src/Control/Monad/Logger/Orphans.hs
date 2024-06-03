092d5e2df09943d516e51b78775684adc98b1d48
o1lo01ol1o <tim.pierson@gmail.com>
2024-06-03 10:18:36 +0100

    conditionally compile orphans

--- src/Control/Monad/Logger/Orphans.hs
+++ src/Control/Monad/Logger/Orphans.hs
@@ -1,17 +1,21 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
-{-|
-Description: Orphan instances for monad-logger's LoggingT
--}
+
+-- |
+-- Description: Orphan instances for monad-logger's LoggingT
 module Control.Monad.Logger.Orphans where
 
+-- add a cpp guard to avoid orphan instances on ghc <= 8.10
+#if !MIN_VERSION_monad_logger(0,3,36)
+import Control.Applicative (Alternative (..))
+import Control.Monad (MonadPlus (..))
 import Control.Monad.Logger
-
-import Control.Applicative (Alternative(..))
-import Control.Monad (MonadPlus(..))
 import Control.Monad.Trans (lift)
+
 
 instance (Monad m, Alternative m) => Alternative (LoggingT m) where
   empty = lift empty
   a <|> b = LoggingT $ \f -> runLoggingT a f <|> runLoggingT b f
 
 instance (Monad m, Alternative m) => MonadPlus (LoggingT m)
+#endif
